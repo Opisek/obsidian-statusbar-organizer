12f95b859622dbbed82e9a6ece722771dc1a7fe5
Opisek <darowski.kacper@gmail.com>
2024-01-14 13:57:57 +0100

    visual drag-and-drop

--- main.ts
+++ main.ts
@@ -2,23 +2,28 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 const ignoredClasses = [
   "mod-clickable",
-  "status-bar-item"
+  "status-bar-item",
+  "statusBarOrganizerSettingsHidden"
 ];
 
 type StatusBarElement = {
-    name: string;
-    index: number;
-    element: Element;
+  name: string;
+  index: number;
+  element: Element;
+  entry?: HTMLDivElement;
 };
 
+type StatusBarElementStatus = {
+  position: number;
+  visible: boolean;
+}
+
 function getStatusBarElements(): StatusBarElement[] {
-  console.log("getting");
   const statusBar = document.getElementsByClassName("status-bar")[0];
   const elements: StatusBarElement[] = [];
   const pluginElementCount: { [key: string]: number } = {};
 
   Array.from(statusBar.children).forEach(element => {
-    console.log(element.className);
     const name = Array
       .from(element.classList)
       .filter(x => !ignoredClasses.contains(x))
@@ -84,6 +89,7 @@ class StatusBarSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
+    // Set up layout
 		const {containerEl} = this;
 
 		containerEl.empty();
@@ -92,16 +98,107 @@ class StatusBarSettingTab extends PluginSettingTab {
     entriesContainer.addClass("statusbarOrganizerEntriesContainer");
     containerEl.appendChild(entriesContainer);
 
-    //const statusBarElementObjects =  
-    const entries = [];
-    for (const statusBarElement of getStatusBarElements()) {
+    // Initialize status
+    const statusBarElements = getStatusBarElements();
+    const elementStatus: { [key: string]: StatusBarElementStatus } = {};
+    for (const [index, statusBarElement] of statusBarElements.entries()) {
+      elementStatus[statusBarElement.name] = {
+        position: index,
+        visible: true
+      };
+    }
+
+    // Functions
+    function toggleVisibility(statusBarElement: StatusBarElement) {
+      const status = elementStatus[statusBarElement.name];
+
+      if (status.visible = !status.visible)
+        statusBarElement.element.removeClass("statusbarOrganizerHidden");
+      else
+        statusBarElement.element.addClass("statusbarOrganizerHidden");
+    }
+
+    let dragging = false;
+
+    function handleMouseDown(statusBarElement: StatusBarElement, event: MouseEvent) {
+      if (dragging) return;
+      dragging = true;
+
+      // Modify real element
+      const realEntry = statusBarElement.entry as HTMLDivElement;
+      realEntry.addClass("statusbarOrganizerClone");
+
+      // Create faux element
+      const fauxEntry = document.createElement("div");
+
+      fauxEntry.addClass("statusbarOrganizerEntry");
+      fauxEntry.addClass("statusbarOrganizerDrag");
+      containerEl.appendChild(fauxEntry);
+
+      fauxEntry.style.left = realEntry.getBoundingClientRect().left + 'px';
+      fauxEntry.style.top = realEntry.getBoundingClientRect().top + 'px';
+      fauxEntry.style.width = realEntry.offsetWidth + 'px';
+      
+      for (const child of Array.from(realEntry.children)) {
+        const fauxSpan = document.createElement("span");
+        fauxSpan.className = child.className;
+        fauxSpan.innerHTML = child.innerHTML;
+        fauxEntry.appendChild(fauxSpan);
+      }
+
+      // Take note of coordinates
+      let offsetX = event.clientX - fauxEntry.getBoundingClientRect().left;
+      let offsetY = event.clientY - fauxEntry.getBoundingClientRect().top;
+      let index = Array.from(entriesContainer.children).indexOf(realEntry);
+
+      //  Handle dragging
+      function handleMouseMove(event: MouseEvent) {
+        fauxEntry.style.left = event.clientX - offsetX + 'px';
+        fauxEntry.style.top = event.clientY - offsetY + 'px';
+
+				const dist = fauxEntry.getBoundingClientRect().top - realEntry.getBoundingClientRect().top;
+
+				if (Math.abs(dist) > realEntry.offsetHeight * 0.75) {
+					const dir = dist / Math.abs(dist);
+
+					var newIndex = Math.max(0, Math.min(index + dir, entriesContainer.children.length - 1));
+					if (newIndex != index) {
+						entriesContainer.removeChild(realEntry);
+						if (newIndex != entriesContainer.children.length)
+              entriesContainer.insertBefore(realEntry, entriesContainer.children[newIndex]);
+            else
+              entriesContainer.appendChild(realEntry);
+
+						index = newIndex;
+					}
+				}
+      }
+
+      window.addEventListener('mousemove', handleMouseMove); 
+
+      // Handle release
+      function handleMouseUp(event: MouseEvent) {
+        realEntry.removeClass("statusbarOrganizerClone");
+        containerEl.removeChild(fauxEntry);
+
+        dragging = false;
+
+        window.removeEventListener('mouseup', handleMouseUp);
+        window.removeEventListener('mousemove', handleMouseMove);
+      }
+      window.addEventListener('mouseup', handleMouseUp);
+    }
+
+    // Generate entries
+    for (const statusBarElement of statusBarElements) {
       const entry = document.createElement("div");
       entry.addClass("statusbarOrganizerEntry");
-      entries.push(entry);
+      statusBarElement.entry = entry;
       entriesContainer.appendChild(entry);
       
       const handle = document.createElement("span");
       handle.addClass("statusbarOrganizerHandle");
+      handle.addEventListener("mousedown", (event) => handleMouseDown(statusBarElement, event));
       entry.appendChild(handle);
 
       const titleSpan = document.createElement("span");
@@ -111,6 +208,7 @@ class StatusBarSettingTab extends PluginSettingTab {
       const visibilitySpan = document.createElement("span");
       visibilitySpan.addClass("statusbarOrganizerVisibility");
       visibilitySpan.innerHTML = "👁";
+      visibilitySpan.onclick = (() => toggleVisibility(statusBarElement));
       entry.appendChild(visibilitySpan);
     }
 
