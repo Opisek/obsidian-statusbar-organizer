b1dbaf814a1ac355e6fbedb452f1e28c6450f17f
Opisek <darowski.kacper@gmail.com>
2024-06-02 20:03:41 +0200

    refactor presets + preset QoL
    - do not allow empty preset names
    - make sure disambiguation does not leave gaps
    - switch to the nearest preset when one is removed

--- src/presets.ts
+++ src/presets.ts
@@ -30,7 +30,7 @@ export async function initializePresets(plugin: StatusBarOrganizer, presetsConta
     // Create preset entry
     const presetEntry = document.createElement("div");
     presetEntry.addClass("statusbar-organizer-preset");
-    presetEntry.id = `statusbar-organizer-preset-${generatePresetId(presetName)}`;
+    presetEntry.id = getPresetId(presetName);
     if (presetName == plugin.settings.activePreset) presetEntry.addClass("statusbar-organizer-preset-active");
     presetsContainer.appendChild(presetEntry);
 
@@ -41,79 +41,151 @@ export async function initializePresets(plugin: StatusBarOrganizer, presetsConta
     nameField.maxLength = 25;
     presetEntry.appendChild(nameField);
 
+    // Preset Renaming
     const renameButton = document.createElement("span");
     renameButton.addClass("statusbar-organizer-preset-delete");
     setIcon(renameButton, "pencil");
-    presetEntry.appendChild(renameButton);
-
-    const deleteButton = document.createElement("span");
-    deleteButton.addClass("statusbar-organizer-preset-delete");
-    setIcon(deleteButton, "x");
-    presetEntry.appendChild(deleteButton);
-
-    // Handle preset selection
-    const switchPreset = async () => {
-      document.getElementById(`statusbar-organizer-preset-${generatePresetId(plugin.settings.activePreset)}`)?.removeClass("statusbar-organizer-preset-active");
-      presetEntry.addClass("statusbar-organizer-preset-active");
-      plugin.settings.activePreset = presetName;
-      await plugin.saveSettings();
-      initializeRows(plugin, settingsContainer);
-    }
-    presetEntry.addEventListener("click", switchPreset);
-
-    // Handle preset renaming
     renameButton.addEventListener("click", async (event) => {
       event.stopPropagation();
-      switchPreset();
+      select();
       nameField.focus();
     });
-
-    const finishedRenaming = async () => {
-      let newName = nameField.value.substring(0, 25).trim();
-      if (newName == presetName) return;
-      if (newName in plugin.settings.presets) {
-        let i = 2;
-        while (`${newName} ${i}` in plugin.settings.presets) i++;
-        newName = `${newName} ${i}`;
-      }
-      nameField.value = newName;
-      if (presetName == plugin.settings.activePreset) plugin.settings.activePreset = newName;
-      presetEntry.id = `statusbar-organizer-preset-${generatePresetId(newName)}`;
-      plugin.settings.presets[newName] = plugin.settings.presets[presetName];
-      delete plugin.settings.presets[presetName];
-      plugin.settings.presetsOrder = plugin.settings.presetsOrder.map(x => x == presetName ? newName : x);
-      plugin.settings.activePreset = newName;
-      presetName = newName;
-      await plugin.saveSettings();
+    const rename = async () => {
+      presetName = await renamePreset(plugin, presetEntry, nameField, presetName);
     }
-    nameField.addEventListener("change", finishedRenaming);
-    nameField.addEventListener("focusout", finishedRenaming);
+    nameField.addEventListener("change", async () => rename());
+    nameField.addEventListener("focusout", async () => rename());
     nameField.addEventListener("input", () => {
       nameField.setAttribute("size", nameField.value.length.toString());
     });
+    presetEntry.appendChild(renameButton);
 
-    // Handle preset deletion
+    // Preset Deletion
+    const deleteButton = document.createElement("span");
+    deleteButton.addClass("statusbar-organizer-preset-delete");
+    setIcon(deleteButton, "x");
     deleteButton.addEventListener("click", async (event) => {
       event.stopPropagation();
-      delete plugin.settings.presets[presetName];
-      plugin.settings.presetsOrder = plugin.settings.presetsOrder.filter(x => x != presetName);
-      await plugin.saveSettings();
-      await initializePresets(plugin, presetsContainer, settingsContainer);
-      await initializeRows(plugin, settingsContainer);
+      await deletePreset(plugin, presetsContainer, settingsContainer, presetName);
     });
+    presetEntry.appendChild(deleteButton);
+
+    // Preset Switching
+    const select = async () => {
+      await switchPreset(plugin, presetEntry, presetName, settingsContainer)
+    }
+    presetEntry.addEventListener("click", async () => select());
   }
 
   // Create a button for adding a new preset
   const newPresetEntry = document.createElement("div");
   newPresetEntry.addClass("statusbar-organizer-preset");
   setIcon(newPresetEntry, "plus");
-  newPresetEntry.onclick = async () => {
-    let i = 1;
-    while (`New Preset ${i}` in plugin.settings.presets) i++;
-    plugin.settings.presets[`New Preset ${i}`] = JSON.parse(JSON.stringify(plugin.settings.presets[plugin.settings.activePreset]));
-    plugin.settings.presetsOrder.push(`New Preset ${i}`);
-    await plugin.saveSettings();
-    await initializePresets(plugin, presetsContainer, settingsContainer);
-  }
+  newPresetEntry.addEventListener("click", () => addPreset(plugin, presetsContainer, settingsContainer));
   presetsContainer.appendChild(newPresetEntry);
+}
+
+/**
+ * Create a new preset cloning the current settings.
+ * 
+ * @param plugin 
+ * @param presetsContainer 
+ * @param settingsContainer 
+ */
+async function addPreset(plugin: StatusBarOrganizer, presetsContainer: HTMLDivElement, settingsContainer: HTMLDivElement) {
+  const presetName = disambiguate("New Preset", plugin.settings.presetsOrder);
+  plugin.settings.presets[presetName] = JSON.parse(JSON.stringify(plugin.settings.presets[plugin.settings.activePreset]));
+  plugin.settings.presetsOrder.push(presetName);
+  plugin.settings.activePreset = presetName;
+  await plugin.saveSettings();
+  await initializePresets(plugin, presetsContainer, settingsContainer);
+}
+
+/**
+ * Delete a preset and switch to the next closest one.
+ * 
+ * @param plugin 
+ * @param presetsContainer 
+ * @param settingsContainer 
+ * @param presetName 
+ */
+async function deletePreset(plugin: StatusBarOrganizer, presetsContainer: HTMLDivElement, settingsContainer: HTMLDivElement, presetName: string) {
+  // Switch to the closest preset
+  const currentIndex = plugin.settings.presetsOrder.indexOf(presetName);
+  if (currentIndex > 0) plugin.settings.activePreset = plugin.settings.presetsOrder[currentIndex - 1];
+  else if (currentIndex < plugin.settings.presetsOrder.length - 1) plugin.settings.activePreset = plugin.settings.presetsOrder[currentIndex + 1];
+  else plugin.settings.activePreset = "Default";
+
+  // Delete preset
+  delete plugin.settings.presets[presetName];
+  plugin.settings.presetsOrder = plugin.settings.presetsOrder.filter(x => x != presetName);
+
+  await plugin.saveSettings();
+  await initializePresets(plugin, presetsContainer, settingsContainer);
+  await initializeRows(plugin, settingsContainer);
+}
+
+/**
+ * Rename a preset.
+ * 
+ * @param plugin 
+ * @param presetEntry 
+ * @param nameField 
+ * @param presetName 
+ * @returns 
+ */
+async function renamePreset(plugin: StatusBarOrganizer, presetEntry: HTMLDivElement, nameField: HTMLInputElement, presetName: string) {
+  // Make sure the name is legal
+  let newName = nameField.value.substring(0, 25).trim();
+  const otherPresets = plugin.settings.presetsOrder.filter(x => x != presetName);
+  newName = (newName == "") ? disambiguate("New Preset", otherPresets) : disambiguate(newName, otherPresets, 2, true);
+  if (newName == presetName) return presetName;
+
+  // Change the name
+  nameField.value = newName;
+  if (presetName == plugin.settings.activePreset) plugin.settings.activePreset = newName;
+  presetEntry.id = getPresetId(newName);
+  plugin.settings.presets[newName] = plugin.settings.presets[presetName];
+  delete plugin.settings.presets[presetName];
+  plugin.settings.presetsOrder = plugin.settings.presetsOrder.map(x => x == presetName ? newName : x);
+  plugin.settings.activePreset = newName;
+
+  await plugin.saveSettings();
+  return newName;
+}
+
+/**
+ * Switch to a different preset.
+*/
+async function switchPreset(plugin: StatusBarOrganizer, presetEntry: HTMLDivElement, presetName: string, settingsContainer: HTMLDivElement) {
+  document.getElementById(getPresetId(plugin.settings.activePreset))?.removeClass("statusbar-organizer-preset-active");
+  presetEntry.addClass("statusbar-organizer-preset-active");
+  plugin.settings.activePreset = presetName;
+  await plugin.saveSettings();
+  await initializeRows(plugin, settingsContainer);
+}
+
+/**
+ * Disambiguate preset names by using numbers.
+ * 
+ * @param presetName 
+ * @param presets 
+ * @param start 
+ * @param allowNoNumber 
+ * @returns 
+ */
+function disambiguate(presetName: string, presets: string[], start: number = 1, allowNoNumber: boolean = false) {
+  if (allowNoNumber && !presets.includes(presetName)) return presetName;
+  while (presets.includes(`${presetName} ${start}`)) start++;
+  return `${presetName} ${start}`;
+}
+
+/**
+ * Generate a preset id as saved in the DOM.
+ * 
+ * @param presetName 
+ * @returns 
+ */
+function getPresetId(presetName: string) {
+  return `statusbar-organizer-preset-${generatePresetId(presetName)}`;
 }
